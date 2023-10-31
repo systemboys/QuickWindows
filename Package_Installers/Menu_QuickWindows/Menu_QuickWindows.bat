bf23cca6df323fc5fb225f1e9f0ff271edee9258
systemboys <systemboys@hotmail.com>
2023-10-31 00:28:17 -0300

    Concatenando variáveis no menu. [test 0.0.5]

--- Package_Installers/Menu_QuickWindows/Menu_QuickWindows.bat
+++ Package_Installers/Menu_QuickWindows/Menu_QuickWindows.bat
@@ -57,7 +57,7 @@ if %ERRORLEVEL% equ 1 (
 
     @REM  Your commands here...
 
-    goto menu_%menuName%
+    goto menu
 )
 
 if %ERRORLEVEL% equ 2 (
@@ -66,7 +66,7 @@ if %ERRORLEVEL% equ 2 (
 
     @REM  Your commands here...
 
-    goto menu_%menuName%
+    goto menu
 )
 
 if %ERRORLEVEL% equ 3 (
@@ -75,7 +75,7 @@ if %ERRORLEVEL% equ 3 (
 
     @REM  Your commands here...
 
-    goto menu_%menuName%
+    goto menu
 )
 
 goto :EOF
