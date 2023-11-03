2891b9f9fddd2af5cc0fc9a89ce5f0b9c504ee68
systemboys <systemboys@hotmail.com>
2023-11-03 00:00:03 -0300

    QuickWindows / Windows | Instalar Winget (tentativa de correção). [0.0.2]

--- Package_Installers/Windows_Session/Install_Winget.ps1
+++ Package_Installers/Windows_Session/Install_Winget.ps1
@@ -13,20 +13,40 @@
 # Licença: GPL.
 
 # Verifica se o Winget já está instalado
-if (-not (Get-Command -Name winget -ErrorAction SilentlyContinue)) {
-    # Link do download
-    $url = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    $output = "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+# if (-not (Get-Command -Name winget -ErrorAction SilentlyContinue)) {
+#     # Link do download
+#     $url = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+#     $output = "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 
-    # Faz o download do instalador do Winget
-    Invoke-WebRequest -Uri $url -OutFile $output
+#     # Faz o download do instalador do Winget
+#     Invoke-WebRequest -Uri $url -OutFile $output
 
-    # Instala o Winget
-    Add-AppxPackage -Path $output -ForceApplicationShutdown
+#     # Instala o Winget
+#     Add-AppxPackage -Path $output -ForceApplicationShutdown
 
-    # Remove o arquivo de instalação
-    Remove-Item -Path $output -Force
+#     # Remove o arquivo de instalação
+#     Remove-Item -Path $output -Force
+# }
+
+# ---
+$url = "https://download.anydesk.com/AnyDesk.exe"
+$outputPath = "C:\Windows\Temp"
+
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadProgressChanged += { 
+    Write-Progress -Activity "Downloading File" -Status "Progress" -PercentComplete $args[1]
 }
+$webClient.DownloadFileCompleted += { 
+    Write-Host "Download completed."
+}
+
+$webClient.DownloadFileAsync($url, $outputPath)
+
+# Aguarda o término do download
+while ($webClient.IsBusy) {
+    Start-Sleep -Milliseconds 100
+}
+# ---
 
 Write-Host "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
