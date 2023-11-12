999cae21312d46a0926e66e49f62ea6a661f7502
systemboys <systemboys@hotmail.com>
2023-11-12 00:34:41 -0300

    Opção para Instalação o Google Chrome. [0.0.4]

--- Package_Installers/Internet_Session/Install_Google_Chrome.ps1
+++ Package_Installers/Internet_Session/Install_Google_Chrome.ps1
@@ -22,7 +22,7 @@ if (Test-Path $directory) {
     Write-Host "Google Chrome is not installed! Starting installation process."
 
     # Link do download e o diretório Temp
-    $downloadUrl = "https://1drv.ms/u/s!AijDX6h8vVT-ioUDaApoWEua8rho0g?e=Uyf4RT"
+    $downloadUrl = "https://public.sn.files.1drv.com/y4mKY8Yd9s9K_w46eB-OOidMe1r98l6xmMZ0HY3gGhBLIJeGrWwsu56sLDOLUluxkiDv0XYa-D-TotfBQ2Kk9xja0stgjbNxfr7mo_EccoPed0F8QiZF2O0d8rLo0VBt2GaIW4ZfoWImA3785aDLNGIev-C66w8l45NOc7AtalEO6n88Q7G-qkZ4NrO9Wbzsm-mKY-bQgXTApNjC2pOTqxpUMyCQcbuxwYCSlyB5aqIrxs?AVOverride=1"
     $downloadPath = "$env:temp\ChromeSetup.exe"
     
     # Faz o download do Google Chrome
