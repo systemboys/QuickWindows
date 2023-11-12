90d93fe7c7d848031187de77ad288dbcc96a124f
systemboys <systemboys@hotmail.com>
2023-11-12 01:47:23 -0300

    Opção para Instalação o Google Chrome. [0.1.2]

--- Package_Installers/Internet_Session/Install_Google_Chrome.ps1
+++ Package_Installers/Internet_Session/Install_Google_Chrome.ps1
@@ -23,8 +23,7 @@ if (Test-Path $directory) {
 
     # Link do download e o diretório Temp
     $downloadUrl = "https://github.com/systemboys/_GTi_Support_/raw/main/Windows/Internet/ChromeSetup.exe"
-    $downloadsPath = [System.IO.Path]::Combine($userProfile, 'Downloads')
-    $downloadPath = "$downloadsPath\ChromeSetup.exe"
+    $downloadPath = "$env:temp\ChromeSetup.exe"
     
     # Faz o download do Google Chrome
     Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
