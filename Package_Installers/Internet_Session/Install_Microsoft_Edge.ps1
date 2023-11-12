379b7b195e7bed59c741f7e3ab4c5b2d83ca98ee
systemboys <systemboys@hotmail.com>
2023-11-12 10:44:53 -0300

    Correções no script que instala o Microsoft Edge. [0.0.1]

--- Package_Installers/Internet_Session/Install_Microsoft_Edge.ps1
+++ Package_Installers/Internet_Session/Install_Microsoft_Edge.ps1
@@ -13,7 +13,24 @@
 # Licença: GPL.
 
 # Instalar o Microsoft Edge, a página de Download será executada.
-Start-Process "https://www.microsoft.com/pt-br/edge"
+# Start-Process "https://www.microsoft.com/pt-br/edge"
+
+# ----------------------------
+Write-Host "Microsoft Edge is not installed! Starting installation process."
+
+# Link do download e o diretório Temp
+$downloadUrl = "https://github.com/systemboys/_GTi_Support_/raw/main/Windows/Internet/MicrosoftEdgeSetup.exe"
+$downloadPath = "$env:temp\MicrosoftEdgeSetup.exe"
+
+# Faz o download do Microsoft Edge
+Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
+
+# Instala o Microsoft Edge
+Start-Process -FilePath "$downloadPath" -Wait
+
+# Apagar o arquivo
+Remove-Item -Path $downloadPath -Force
+# ----------------------------
 
 Write-Host "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
