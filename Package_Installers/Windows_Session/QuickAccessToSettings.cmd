e47b6c1c8cb8d53b654cf40285c5725e1771195e
systemboys <systemboys@hotmail.com>
2023-11-14 01:11:09 -0300

    Opção para Menu de sessão a acesso rápido a algumas funcionalidades do Windows na sessão 'Windows'. [0.1.1]

--- Package_Installers/Windows_Session/QuickAccessToSettings.cmd
+++ Package_Installers/Windows_Session/QuickAccessToSettings.cmd
@@ -39,6 +39,11 @@ set "menu_Session_2_1[3]=Configurações do Sistema (MSConfig)"
 set "menu_Session_2_1[4]=Serviços (Services.msc)"
 set "menu_Session_2_1[5]=Gerenciador de Dispositivos (DevMgmt.msc)"
 set "menu_Session_2_1[6]=Gerenciamento de Discos (DiskMgmt.msc)"
+set "menu_Session_2_1[7]=Editar Configurações do Plano"
+set "menu_Session_2_1[8]=Explorador de arquivos do Windows"
+set "menu_Session_2_1[9]=Configurações - Tela (tipo, ajustes de resolução de tela)"
+set "menu_Session_2_1[10]=Sobre o Windows (WinVer)"
+set "menu_Session_2_1[11]=Configurações avançadas do sistema (Propriedades do Sistema)"
 
 set "default=0"
 
@@ -99,6 +104,51 @@ if %ERRORLEVEL% equ 6 (
     echo Você selecionou a Opção Gerenciamento de Discos - DiskMgmt.msc.
 
     diskmgmt.msc
+
+    goto menu_Session_2_1
+)
+
+if %ERRORLEVEL% equ 7 (
+    cls
+    echo Você selecionou a Opção Editar Configurações do Plan.
+
+    powercfg.cpl
+
+    goto menu_Session_2_1
+)
+
+if %ERRORLEVEL% equ 8 (
+    cls
+    echo Você selecionou a Opção Explorador de arquivos do Window.
+
+    explorer
+
+    goto menu_Session_2_1
+)
+
+if %ERRORLEVEL% equ 9 (
+    cls
+    echo Você selecionou a Opção Configurações - Tela - tipo, ajustes de resolução de tela.
+
+    desk.cpl
+
+    goto menu_Session_2_1
+)
+
+if %ERRORLEVEL% equ 10 (
+    cls
+    echo Você selecionou a Opção Sobre o Windows - WinVer.
+
+    winver
+
+    goto menu_Session_2_1
+)
+
+if %ERRORLEVEL% equ 11 (
+    cls
+    echo Você selecionou a Opção Configurações avançadas do sistema - Propriedades do Sistema.
+
+    sysdm.cpl
 
     goto menu_Session_2_1
 )
