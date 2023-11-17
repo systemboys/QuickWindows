6f11f8bf22af07cf644efe1e373f505e7c68203f
systemboys <systemboys@hotmail.com>
2023-11-17 17:59:20 -0300

    Foi criado um novo arquivo 'QuickWindows.ps1' para substituir o arquivo 'QuickWindows.cmd', agora todo o script será em PowerShell. O layout do menu foi mudado radicalmente. [0.0.6]

--- qw/globalFunctions.ps1
+++ qw/globalFunctions.ps1
@@ -6,8 +6,8 @@ function commandExecution_0() {
 function commandExecution_1() {
     Write-Host "Running commands for $selection"
     # Start of commands here...
-    cd .\Package_Installers\Menu_QuickWindows
-    & .\Menu_QuickWindows.ps1
+    $scriptPath = "Package_Installers\Menu_QuickWindows\Menu_QuickWindows.ps1"
+    Invoke-Expression -Command "& '$scriptPath'"
     # End of commands here...
     Read-Host -Prompt "Commands executed successfully, press Enter to return!"
     $defaultSelection = 1
