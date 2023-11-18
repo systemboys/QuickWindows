b6409d5d23c92efc57f356a791d1641e3bf8d14a
systemboys <systemboys@hotmail.com>
2023-11-17 22:03:35 -0300

    Foi criado um novo arquivo 'QuickWindows.ps1' para substituir o arquivo 'QuickWindows.cmd', agora todo o script será em PowerShell. O layout do menu foi mudado radicalmente. [0.2.5]

--- qw/QuickWindows.ps1
+++ qw/QuickWindows.ps1
@@ -81,7 +81,45 @@
 . .\inclusionFunctions.ps1
 
 # My functions
-. .\globalFunctions.ps1
+# Functions of choices after Enter
+function commandExecution_0() {
+    clear
+    exit
+}
+
+function commandExecution_1() {
+    Write-Host "Running commands for $selection"
+    # Start of commands here...
+    & .\Package_Installers\Menu_QuickWindows\Menu_QuickWindows.ps1
+    # End of commands here...
+    Read-Host -Prompt "Commands executed successfully, press Enter to return!"
+    $defaultSelection = 1
+    & .\QuickWindows.ps1 $defaultSelection
+}
+
+function commandExecution_2() {
+    Write-Host "Running commands for $selection"
+    # Start of commands here...
+    # Command 1...
+    # Command 2...
+    # Command 3...
+    # End of commands here...
+    Read-Host -Prompt "Commands executed successfully, press Enter to return!"
+    $defaultSelection = 2
+    & .\QuickWindows.ps1 $defaultSelection
+}
+
+function commandExecution_3() {
+    Write-Host "Running commands for $selection"
+    # Start of commands here...
+    # Command 1...
+    # Command 2...
+    # Command 3...
+    # End of commands here...
+    Read-Host -Prompt "Commands executed successfully, press Enter to return!"
+    $defaultSelection = 3
+    & .\QuickWindows.ps1 $defaultSelection
+}
 
 # Adjusting PowerShell window dimensions
 $width = "120"
