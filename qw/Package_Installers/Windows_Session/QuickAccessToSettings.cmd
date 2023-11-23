8100fd04bd59f71124f10658555e4bef16ab957a
systemboys <systemboys@hotmail.com>
2023-11-23 19:15:41 -0300

    ## Menu interativo com PowerShell, uma maneira organizada de organizar opções. [0.4.8]

--- qw/Package_Installers/Windows_Session/QuickAccessToSettings.cmd
+++ qw/Package_Installers/Windows_Session/QuickAccessToSettings.cmd
@@ -1,37 +1,10 @@
-<# : Batch portion
-@echo off & setlocal enabledelayedexpansion
-
-:: QuickAccessToSettings.cmd - Para executar algumas funcionalidades do Windows.
-::
-:: Autor: Marcos Aurélio R. da Silva "systemboys@hotmail.com"
-:: Manutenção: Marcos Aurélio R. da Silva "systemboys@hotmail.com"
-::
-:: ---------------------------------------------------------------
-:: Este programa tem a finalidade de dar acesso rápido a algumas funcionalidades do Windows.
-:: ---------------------------------------------------------------
-:: Histórico:
-:: v0.0.1 2023-11-14 às 00h15, Marcos Aurélio:
-::   - Versão inicial, Menu de sessão a acesso rápido a algumas funcionalidades do Windows.
-::
-:: Licença: GPL.
+@echo off
+setlocal enabledelayedexpansion
 
 cls
-
 chcp 65001 > nul
 
-:: Obter o ano atual
-for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
-set "ano=%datetime:~0,4%"
-
-:: Subtrair o ano atual por 2008
-set /a resultado=ano-2008
-
-:: Mensagem de entrada do Menu com o resultado
-echo © %ano% - GLOBAL TEC Informática ® - A %resultado% no mercado de Informática.
-echo www.gti1.com.br - gti.inf@hotmail.com - systemboys@hotmail.com
-echo QuickWindows / Access Settings
-
-:: Opções do Menu
+:: Definição das opções do menu
 set "menu_Session_2_1[0]=Voltar..."
 set "menu_Session_2_1[1]=Painel de Controle (Control)"
 set "menu_Session_2_1[2]=Editor de Registro (RegEdit)"
@@ -42,188 +15,46 @@ set "menu_Session_2_1[6]=Gerenciamento de Discos (DiskMgmt.msc)"
 set "menu_Session_2_1[7]=Explorador de arquivos do Windows"
 set "menu_Session_2_1[8]=Configurações - Tela (tipo, ajustes de resolução de tela)"
 set "menu_Session_2_1[9]=Configurações avançadas do sistema (Propriedades do Sistema)"
+set "menu_Session_2_1[10]=Nova Opção"
+set "menu_Session_2_1[11]=Outra Opção"
+set "menu_Session_2_1[12]=Mais uma Opção"
 
 set "default=0"
 
-:menu_Session_2_1
-powershell -noprofile "iex (gc \"%~f0\" | out-string)"
-if %ERRORLEVEL% equ 0 (
-    cls
-    call Windows_Session.cmd
+:display_menu
+cls
+echo © %date:~6,4% - GLOBAL TEC Informática ® - A !date:~6,4! no mercado de Informática.
+echo www.gti1.com.br - gti.inf@hotmail.com - systemboys@hotmail.com
+echo QuickWindows / Access Settings
+
+set "menu_Session_2_1title=== QuickWindows / Windows / Access Settings ==="
+echo %menu_Session_2_1title%
+echo Use as teclas direcionais. Pressione Enter para selecionar.
+
+set /a maxlen=0
+for /l %%a in (0,1,12) do (
+    set "option=!menu_Session_2_1[%%a]!"
+    if defined option (
+        set /a len=9+%%a
+        if !len! gtr !maxlen! set maxlen=!len!
+        echo %%a: !option!
+    )
 )
 
-if %ERRORLEVEL% equ 1 (
-    cls
-    echo Você selecionou a Opção Painel de Controle - Control.
+set /a selection=%default%
 
-    control
+:read_key
+choice /c 0123456789ABCDEFGHIJ /n >nul
+set "key=!errorlevel!"
+if %key% geq 48 (
+    set /a selection=%key%-48
+    goto execute_option
+) else if %key% geq 13 goto execute_option
+goto read_key
 
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 2 (
-    cls
-    echo Você selecionou a Opção Editor de Registro - RegEdit.
-
-    regedit
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 3 (
-    cls
-    echo Você selecionou a Opção Configurações do Sistema - MSConfig.
-
-    msconfig
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 4 (
-    cls
-    echo Você selecionou a Opção Serviços - Services.msc.
-
-    services.msc
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 5 (
-    cls
-    echo Você selecionou a Opção Gerenciador de Dispositivos - DevMgmt.msc.
-
-    devmgmt.msc
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 6 (
-    cls
-    echo Você selecionou a Opção Gerenciamento de Discos - DiskMgmt.msc.
-
-    diskmgmt.msc
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 7 (
-    cls
-    echo Você selecionou a Opção Explorador de arquivos do Window.
-
-    explorer
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 8 (
-    cls
-    echo Você selecionou a Opção Configurações - Tela - tipo, ajustes de resolução de tela.
-
-    desk.cpl
-
-    goto menu_Session_2_1
-)
-
-if %ERRORLEVEL% equ 9 (
-    cls
-    echo Você selecionou a Opção Configurações avançadas do sistema - Propriedades do Sistema.
-
-    sysdm.cpl
-
-    goto menu_Session_2_1
-)
-
-goto :EOF
-: end batch / begin PowerShell hybrid chimera #>
-
-$menu_Session_2_1title = "=== QuickWindows / Windows / Access Settings ==="
-$menu_Session_2_1prompt = "Use as teclas direcionais. Pressione Enter para selecionar."
-
-$maxlen = $menu_Session_2_1prompt.length + 6
-$menu_Session_2_1 = gci env: | ?{ $_.Name -match "^menu_Session_2_1\[\d+\]$" } | %{
-    $_.Value.trim()
-    $len = $_.Value.trim().Length + 6
-    if ($len -gt $maxlen) { $maxlen = $len }
-}
-[int]$selection = $env:default
-$h = $Host.UI.RawUI.WindowSize.Height
-$w = $Host.UI.RawUI.WindowSize.Width
-$xpos = [math]::floor(($w - ($maxlen + 5)) / 2)
-$ypos = [math]::floor(($h - ($menu_Session_2_1.Length + 4)) / 3)
-
-$offY = [console]::WindowTop;
-$rect = New-Object Management.Automation.Host.Rectangle `
-    0,$offY,($w - 1),($offY+$ypos+$menu_Session_2_1.length+4)
-$buffer = $Host.UI.RawUI.GetBufferContents($rect)
-
-function destroy {
-    $coords = New-Object Management.Automation.Host.Coordinates 0,$offY
-    $Host.UI.RawUI.SetBufferContents($coords,$buffer)
-}
-
-function getKey {
-    while (-not ((37..40 + 13 + 48..(47 + $menu_Session_2_1.length)) -contains $x)) {
-        $x = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode
-    }
-    $x
-}
-
-# http://goo.gl/IAmdR6
-function WriteTo-Pos ([string]$str, [int]$x = 0, [int]$y = 0,
-    [string]$bgc = [console]::BackgroundColor, [string]$fgc = [Console]::ForegroundColor) {
-    if($x -ge 0 -and $y -ge 0 -and $x -le [Console]::WindowWidth -and
-        $y -le [Console]::WindowHeight) {
-        $saveY = [console]::CursorTop
-        $offY = [console]::WindowTop       
-        [console]::setcursorposition($x,$offY+$y)
-        Write-Host $str -b $bgc -f $fgc -nonewline
-        [console]::setcursorposition(0,$saveY)
-    }
-}
-
-function center([string]$what) {
-    $what = "    $what  "
-    $lpad = " " * [math]::max([math]::floor(($maxlen - $what.length) / 2), 0)
-    $rpad = " " * [math]::max(($maxlen - $what.length - $lpad.length), 0)
-    WriteTo-Pos "$lpad   $what   $rpad" $xpos $line blue yellow
-}
-
-function menu_Session_2_1 {
-    $line = $ypos
-    center $menu_Session_2_1title
-    $line++
-    center " "
-    $line++
-
-    for ($i=0; $item = $menu_Session_2_1[$i]; $i++) {
-        # write-host $xpad -nonewline
-        $rtpad = " " * ($maxlen - $item.length)
-        if ($i -eq $selection) {
-            WriteTo-Pos "  > $item <$rtpad" $xpos ($line++) yellow blue
-        } else {
-            WriteTo-Pos " $i`: $item  $rtpad" $xpos ($line++) blue yellow
-        }
-    }
-    center " "
-    $line++
-    center $menu_Session_2_1prompt
-    1
-}
-
-while (menu_Session_2_1) {
-
-    [int]$key = getKey
-
-    switch ($key) {
-
-        37 {}   # left or up
-        38 { if ($selection) { $selection-- }; break }
-
-        39 {}   # right or down
-        40 { if ($selection -lt ($menu_Session_2_1.length - 1)) { $selection++ }; break }
-
-        # number or enter
-        default { if ($key -gt 13) {$selection = $key - 48}; destroy; exit($selection) }
-    }
-}
-
+:execute_option
+cls
+echo Você selecionou a Opção: !menu_Session_2_1[%selection%]!
+rem Aqui você executa a ação da opção selecionada
+pause
+goto display_menu
