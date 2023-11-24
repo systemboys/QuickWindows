27f31c4571076dbebd2feca2f1decc9a41535437
systemboys <systemboys@hotmail.com>
2023-11-23 21:19:32 -0300

    ## Menu interativo com PowerShell, uma maneira organizada de organizar opções. [0.7.4]

--- qw/optionFunctions.ps1
+++ qw/optionFunctions.ps1
@@ -26,8 +26,8 @@ function menuOption_1() {
     Write-Host " Function 1 executed successfully..."
 
     # Start your commands here
-    exit
     & .\Package_Installers\Menu_QuickWindows\Menu_QuickWindows.ps1
+    exit
     # End your commands here
 
     # Press a key to continue...
