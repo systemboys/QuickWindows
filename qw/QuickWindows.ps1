afb3acc826b652927274a9f400344b24fa2527f6
systemboys <systemboys@hotmail.com>
2023-11-28 19:24:38 -0300

    Novo teste de menu interativo. [0.0.1]

--- qw/QuickWindows.ps1
+++ qw/QuickWindows.ps1
@@ -77,159 +77,125 @@ v0.2.8 2023-11-14 às 18h02, Marcos Aurélio:
 Licença: GPL.
 #>
 
-# Get the name of the current script file
-$currentFileName = $MyInvocation.MyCommand.Name
-
 # Adjusting PowerShell window dimensions
 $width = "120"
 $height = "30"
 $size = New-Object System.Management.Automation.Host.Size($width, $height)
 $host.UI.RawUI.WindowSize = $size
 
+# Definition of variables
+$currentYear = Get-Date -Format yyyy
+$timeOnMarket = ($currentYear - 2008)
+
 # Colors
 $Host.UI.RawUI.BackgroundColor = "Black" # Background
 $Host.UI.RawUI.ForegroundColor = "Green" # Font
 
-# Optoin Functions
-. .\optionFunctions.ps1
-
-# Globla Variables
-. .\globalVariables.ps1
-
-# Option Menu
-. .\menuOptions.ps1
-
-# Region FUNCTIONS
-
-# Function used to simply revert console colors
-function Reverse-Colors {
-    $bColor = [System.Console]::BackgroundColor
-    $fColor = [System.Console]::ForegroundColor
-    [System.Console]::BackgroundColor = $fColor
-    [System.Console]::ForegroundColor = $bColor    
+# Menu Option 0
+function menuOption_0() {
+    Stop-Process -Name powershell
 }
 
-# Main function showing the menu
-function New-Menu {
-    param(
-        [parameter(Mandatory=$true)][System.Collections.Generic.List[string]]$menuItems, # Contains all menu items
-        [string]$title       = $title,                                                   # The title for the menu
-        [string]$hint        = $hint,                                                    # Hint to be displayed above menu entries
-        [string]$footer      = $footer,                                                  # Menu footer
-        [ValidateSet("green","yellow","red","black","white")]                            # You might add more colors allowed by console
-        [string]$titleColor  = 'Yellow'                                                  # Color of the title
-    )
-    
-    # Prepare variables with function wide scope
-    $invalidChoice = $false                     # Initialize the flag indicating whether an ivalid key was pressed
-    $selectIndex   = 0                          # Initialize the variable storing the selection index (by default the first entry)
-    $outChar       = 'a'                        # Initialize the variable storing the Enter or Esc value
+# Menu Option "Menu QuickWindows"
+function menuOption_1() {
+    Write-Host " Function 1 executed successfully..."
 
-    # Prepare the cosnole
-    [System.Console]::CursorVisible = $false    # Hide the cursor, we don't need it
-    [Console]::Clear()                          # Clear everything before showing the menu
+    # Start your commands here
+    & .\Package_Installers\Menu_QuickWindows\Menu_QuickWindows.ps1
+    # End your commands here
 
-    # Main loop showing all the entries and handling the interaction with user
-    # End the loop only when Enter or Escape is pressed
-    while (([System.Int16]$inputChar.Key -ne [System.ConsoleKey]::Enter) -and ([System.Int16]$inputChar.Key -ne [System.ConsoleKey]::Escape)) {
-        
-        # Show title and hint
-        [System.Console]::CursorTop = 0                     # Start from top and then overwrite all lines; it's used instead of Clear to avoid blinking
-        $tempColor = [System.Console]::ForegroundColor      # Keep the default font color 
-        [System.Console]::ForegroundColor = $titleColor     # Set the color for title according to value of parameter
-        [System.Console]::WriteLine("$title`n")             # The title for the menu
-        [System.Console]::ForegroundColor = $tempColor      # Revert back to default font color
-        
-        [System.Console]::WriteLine($hint)
-        [System.Console]::WriteLine($header)
-        # Show all entries
-        for ($i = 0; $i -lt $menuItems.Count; $i++) {
-            [System.Console]::Write("$leftSideEdge > [$i] ")                    # Add identity number to each entry, it's not highlighted for selection but it's in the same line
-            if ($selectIndex -eq $i) {
-                Reverse-Colors                                      # In case this is the selected entry, reverse color just for it to make the selection visible
-                [System.Console]::WriteLine($menuItems[$i] + "< $rightSideEdge")
-                Reverse-Colors      
-            } else {
-                [System.Console]::WriteLine($menuItems[$i] + "< $rightSideEdge") # In case this is not-selected entry, just show it
-            }
-        }
-        [System.Console]::WriteLine($footer)
-
-        # In case of invalid key, show the message
-        if ($invalidChoice) {
-            [System.Console]::WriteLine("Invalid button! Try again...")
-        } else {
-            [System.Console]::Write([System.String]::new(' ',[System.Console]::WindowWidth)) # In case the valid key was used after invalid, clean-up this line
-            [System.Console]::SetCursorPosition(0,[System.Console]::CursorTop)               # Set the cursor back to first column so it's properly back to 1st column, 1st row in next iteration of the loop
-        }
-        $invalidChoice = $false                                                              # Reset the invalid key flag
-
-        # Read the key from user
-        $inputChar=[System.Console]::ReadKey($true)
-
-        # Try to convert it to number
-        try {
-            $number = [System.Int32]::Parse($inputChar.KeyChar)
-        }
-        catch{
-            $number = -1                                                                     # In case it's not a valid number, set to always invalid -1
-        }
-        
-        # Hanlde arrows
-        if ([System.Int16]$inputChar.Key -eq [System.ConsoleKey]::DownArrow) {
-            if ($selectIndex -lt $menuItems.Count -1) {                                       # Avoid selection out of range
-                $selectIndex++
-            }
-        } elseif ([System.Int16]$inputChar.Key -eq [System.ConsoleKey]::UpArrow) {
-            if ($selectIndex -gt 0) {                                                         # Avoid selection out of range
-                $selectIndex--
-            }
-        } elseif ($number -ge 0 -and $number -lt $menuItems.Count) {                          # If it's valid number within the range
-            # Handle double-digit numbers
-            $timestamp = Get-Date       
-            while (![System.Console]::KeyAvailable -and ((get-date) - $timestamp).TotalMilliseconds -lt 500) {
-                Start-Sleep -Milliseconds 250                                               # Give the user 500 miliseconds to type in the 2nd digit, check after 250 to improve responsivness
-            }
-            if ([System.Console]::KeyAvailable) {                                            # If user typed a key, read it in next line
-                $secondChar = [System.Console]::ReadKey($true).KeyChar
-                $fullChar   = "$($inputChar.KeyChar)$($secondChar)"                         # Join both keys
-                try {
-                    # Set selection
-                    $number = [System.Int32]::Parse($fullChar)                              # Set the selection accordingly or raise flag for invalid key
-                    if ($number -ge 0 -and $number -lt $menuItems.Count) {
-                        $selectIndex   = $number
-                    } else {
-                        $invalidChoice = $true
-                    }                
-                }
-                catch {
-                    $invalidChoice = $true
-                }
-            } else {
-                # Set selection
-                $selectIndex = $number                                                       # Set selection for single digit number
-            }
-        } else {
-            $invalidChoice = $true                                                           # Key not recognized, raise the flag
-        }
-        $outChar = $inputChar                                                                # Assign the key value to variable with scope outside the loop
-    }
-
-    # Hanlde the result, just show the selected entry if Enter was pressed; do nothing if Escape was pressed
-    if ($outChar.Key -eq [System.ConsoleKey]::Enter) {
-        [Console]::WriteLine(" You selected $($menuItems[$selectIndex])")
-        Invoke-Command -ScriptBlock (Get-Command "menuOption_$selectIndex").ScriptBlock
-    }
+    # Press a key to continue...
+    Write-Host " Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 }
 
-# Endregion FUNCTIONS
+# Menu Option 2
+function menuOption_2() {
+    Write-Host " Function 2 executed successfully..."
 
-# Region MAIN SCRIPT
+    # Start your commands here
+    # Command 1...
+    # Command 2...
+    # Command 3...
+    # End your commands here
 
-# Show the menu
-do {
-    New-Menu $menuItems
-} while ($outChar.Key -ne [System.ConsoleKey]::Escape)
+    # Press a key to continue...
+    Write-Host " Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+# Menu Option 3
+function menuOption_3() {
+    Write-Host " Function 3 executed successfully..."
+
+    # Start your commands here
+    # Command 1...
+    # Command 2...
+    # Command 3...
+    # End your commands here
+
+    # Press a key to continue...
+    Write-Host " Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+$menutitle = "                     ┌───────────────────────────┐                      
+ ┌───────────────────┤░▒▓ QuackWindows | Home ▓▒░├────────────────────┐ 
+┌┴───────────────────┘                           └────────────────────┴┐"
+$menuprompt = "└┬──────────────────────────────────────────────────────────────┬─┬─┬─┬┘
+ │ (C) $currentYear GLOBAL TEC Informática (R) - GTi                      - ┼ ┤ 
+ │ A $timeOnMarket anos no mercado de informática.                             - ┤ 
+ │ A Tecnologia da Informção é o Futuro.                              ┤ 
+ │ Website: https://gti1.com.br | Email: systemboys@hotmail.com     - ┤ 
+ │ Author: Marcos Aurélio - Engenheiro de Software                - ┼ ┤ 
+ └──────────────────────────────────────────────────────────────┴─┴─┴─┘ "
+$backgroundHeader = "DarkGreen"
+$backgroundFooter = "DarkGreen"
+$backgroundMenu = "DarkMagenta"
+$backgroundSelect = "DarkGreen"
+$fontColor = "Green"
+$fontColorSelect = "Green"
+
+# Definição das opções do menu
+$menu = @(
+    "..\Sair                                                            ",
+    "Option 1                                                           ",
+    "Option 2                                                           "
+    "Option 3                                                           "
+)
+
+$default = 0
+
+function Show-Menu {
+    Clear-Host
+    Write-Host $menutitle -BackgroundColor $backgroundHeader -ForegroundColor $fontColor
+
+    for ($i = 0; $i -lt $menu.Count; $i++) {
+        if ($i -eq $selection) {
+            Write-Host ("│" + $i + ": " + $menu[$i] + "│") -BackgroundColor $backgroundSelect -ForegroundColor $fontColorSelect
+        } else {
+            Write-Host ("│" + $i + ": " + $menu[$i] + "│") -BackgroundColor $backgroundMenu -ForegroundColor $fontColor
+        }
+    }
+
+    Write-Host $menuprompt -BackgroundColor $backgroundFooter -ForegroundColor $fontColor
+}
+
+$selection = $default
+
+while ($true) {
+    Show-Menu
+
+    $key = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode
+
+    switch ($key) {
+        38 { if ($selection -gt 0) { $selection-- } } # up arrow
+        40 { if ($selection -lt ($menu.Count - 1)) { $selection++ } } # down arrow
+        13 { # Enter key
+            Invoke-Command -ScriptBlock (Get-Command "menuOption_$selection").ScriptBlock
+        }
+    }
+}
 
 #endregion MAIN SCRIPT
 
