9347697a5a7c1715d8e23de65c931bb14b150797
systemboys <systemboys@hotmail.com>
2023-11-29 23:24:41 -0300

    Utilitários para Windows. [0.4.1]

--- Package_Installers/RunPowerShellCommands.ps1
+++ Package_Installers/RunPowerShellCommands.ps1
@@ -17,12 +17,24 @@
 # Licença: GPL.
 
 # Script de Interação: Janela de Comando Interativa para Execução de Comandos
+function ExecutarComando {
+    param (
+        [string]$comando
+    )
+
+    # Abrir uma nova janela do PowerShell e executar o comando
+    $psi = New-Object System.Diagnostics.ProcessStartInfo
+    $psi.FileName = "powershell"
+    $psi.Arguments = "-NoExit -Command `"$comando`""
+    [System.Diagnostics.Process]::Start($psi) | Out-Null
+}
+
 Add-Type -AssemblyName System.Windows.Forms
 
 # Cria uma janela de formulário
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Executar Comando"
-$form.Width = 300
+$form.Width = 350
 $form.Height = 150
 $form.StartPosition = "CenterScreen"
 
@@ -37,6 +49,13 @@ $form.Controls.Add($label)
 $textBox = New-Object System.Windows.Forms.TextBox
 $textBox.Location = New-Object System.Drawing.Point(50,30)
 $textBox.Size = New-Object System.Drawing.Size(200,20)
+$textBox.Text = "Digite o comando aqui..."
+$textBox.Add_Enter({
+    # Limpa o texto inicial quando o usuário clicar no campo
+    if ($textBox.Text -eq "Digite o comando aqui...") {
+        $textBox.Text = ""
+    }
+})
 $form.Controls.Add($textBox)
 
 # Cria um botão "Enviar"
@@ -46,12 +65,12 @@ $button.Size = New-Object System.Drawing.Size(100,23)
 $button.Text = "Enviar"
 $button.Add_Click({
     # Verifica se o campo está vazio
-    if ([string]::IsNullOrWhiteSpace($textBox.Text)) {
-        [System.Windows.Forms.MessageBox]::Show("Por favor, digite um comando!", "Campo Vazio", "OK", [System.Windows.Forms.MessageBoxIcon]::Warning)
+    if ([string]::IsNullOrWhiteSpace($textBox.Text) -or $textBox.Text -eq "Digite o comando aqui...") {
+        [System.Windows.Forms.MessageBox]::Show("O campo é obrigatório. Por favor, digite um comando.", "Campo Vazio", "OK", [System.Windows.Forms.MessageBoxIcon]::Warning)
     } else {
-        # Quando o botão for clicado e o campo não estiver vazio, executa o comando no PowerShell
+        # Quando o botão for clicado e o campo não estiver vazio, executa o comando na nova janela do PowerShell
         $command = $textBox.Text
-        Invoke-Expression $command
+        ExecutarComando -comando $command
     }
 })
 $form.Controls.Add($button)
