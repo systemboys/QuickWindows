1d9b13cba6ef0ed178772e1757e9eb2f0d2b6e96
systemboys <systemboys@hotmail.com>
2023-11-29 22:46:00 -0300

    Utilitários para Windows. [0.3.4]

--- Package_Installers/RunPowerShellCommands.ps1
+++ Package_Installers/RunPowerShellCommands.ps1
@@ -17,8 +17,40 @@
 # Execução Interativa de Comandos no PowerShell
 
 # Solicita ao usuário que digite um comando por meio do cmdlet
-$command = Read-Host "Enter a command to be executed in PowerShell"
-Invoke-Expression $command
+# $command = Read-Host "Enter a command to be executed in PowerShell"
+# Invoke-Expression $command
+
+# ------------------------------------------
+Add-Type -AssemblyName System.Windows.Forms
+
+# Cria uma janela de formulário
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Executar Comando"
+$form.Width = 300
+$form.Height = 150
+$form.StartPosition = "CenterScreen"
+
+# Cria um campo de texto para o comando
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(50,30)
+$textBox.Size = New-Object System.Drawing.Size(200,20)
+$form.Controls.Add($textBox)
+
+# Cria um botão "Enviar"
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(100,70)
+$button.Size = New-Object System.Drawing.Size(100,23)
+$button.Text = "Enviar"
+$button.Add_Click({
+    # Quando o botão for clicado, executa o comando no PowerShell
+    $command = $textBox.Text
+    Invoke-Expression $command
+})
+$form.Controls.Add($button)
+
+# Mostra o formulário
+$form.ShowDialog()
+# ------------------------------------------
 
 Write-Host "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
