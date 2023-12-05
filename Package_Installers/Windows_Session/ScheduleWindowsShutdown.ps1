2e16dbfd0564fa2df65af0aaac108fa3cdf48946
systemboys <systemboys@hotmail.com>
2023-12-05 17:35:54 -0300

    Opção para agendar desligamento automático na sessão 'Windows'. [0.1.8]

--- Package_Installers/Windows_Session/ScheduleWindowsShutdown.ps1
+++ Package_Installers/Windows_Session/ScheduleWindowsShutdown.ps1
@@ -12,18 +12,32 @@
 #
 # Licença: GPL.
 
-# Verifica se um desligamento foi agendado
-$shutdownScheduled = (schtasks /query /TN "Microsoft\Windows\Shutdown" | Select-String "Pronto").Count -gt 0
-
-# Define a variável com base no resultado
-if ($shutdownScheduled) {
-    $shutdownVariable = 1
-} else {
-    $shutdownVariable = 0
-}
-
-# Imprime o valor da variável
-Write-Output $shutdownVariable
+do {
+    Write-Host "0 = Exit"
+    Write-Host "1 = Schedule a shutdown"
+    Write-Host "2 = Cancel a shutdown"
+    $input = Read-Host "Please choose an option"
+    switch ($input) {
+        '0' {
+            exit
+        }
+        '1' {
+            $time = Read-Host "Please enter the time in minutes for the shutdown"
+            if (![string]::IsNullOrWhiteSpace($time) -and $time -match '^\d+$') {
+                $seconds = $time * 60
+                shutdown -s -t $seconds
+            } else {
+                Write-Host "Invalid input. Please enter a valid number of minutes."
+            }
+        }
+        '2' {
+            shutdown -a
+        }
+        default {
+            Write-Host "Invalid option. Please choose a valid option."
+        }
+    }
+} while ($input -ne '0')
 
 Write-Host "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
