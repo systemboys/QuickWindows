6814fdb6512425dbbfb37e0ea4f7db7cbed6a821
systemboys <systemboys@hotmail.com>
2023-12-11 00:21:46 -0300

    Opção para colocar URL e iniciar download direto a partir do Windows PowerShell na sessão 'Internet / Downloads'. [0.0.8]

--- Package_Installers/Internet_Session/DownloadURL.ps1
+++ Package_Installers/Internet_Session/DownloadURL.ps1
@@ -6,11 +6,7 @@ $dest = Read-Host
 
 if (![string]::IsNullOrWhiteSpace($url) -and ![string]::IsNullOrWhiteSpace($dest)) {
     Write-Host "Iniciando o download..."
-    $fileName = [System.IO.Path]::GetFileName($url)
-    $extension = [System.IO.Path]::GetExtension($fileName)
-    $destWithExtension = Join-Path -Path $dest -ChildPath $fileName
-
-    Start-BitsTransfer -Source $url -Destination $destWithExtension
+    Start-BitsTransfer -Source $url -Destination $dest
 } else {
     Write-Host "URL e destino são obrigatórios."
 }
