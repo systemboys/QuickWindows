f36280c0608a3abe0077f6006f2770842213fc59
systemboys <systemboys@hotmail.com>
2023-12-11 00:28:28 -0300

    Opção para colocar URL e iniciar download direto a partir do Windows PowerShell na sessão 'Internet / Downloads'. [0.0.9]

--- Package_Installers/Internet_Session/DownloadURL.ps1
+++ Package_Installers/Internet_Session/DownloadURL.ps1
@@ -1,12 +1,28 @@
-Write-Host "Digite a URL:"
-$url = Read-Host
+$validInput = $false
 
-Write-Host "Digite o destino:"
-$dest = Read-Host
+while (-not $validInput) {
+    Write-Host "Digite a URL:"
+    $url = Read-Host
 
-if (![string]::IsNullOrWhiteSpace($url) -and ![string]::IsNullOrWhiteSpace($dest)) {
-    Write-Host "Iniciando o download..."
-    Start-BitsTransfer -Source $url -Destination $dest
-} else {
-    Write-Host "URL e destino são obrigatórios."
+    if (![string]::IsNullOrWhiteSpace($url)) {
+        $validInput = $true
+    } else {
+        Write-Host "URL é obrigatória. Por favor, digite uma URL válida."
+    }
 }
+
+$validInput = $false
+
+while (-not $validInput) {
+    Write-Host "Digite o destino:"
+    $dest = Read-Host
+
+    if (![string]::IsNullOrWhiteSpace($dest)) {
+        $validInput = $true
+    } else {
+        Write-Host "Destino é obrigatório. Por favor, digite um destino válido."
+    }
+}
+
+Write-Host "Iniciando o download..."
+Start-BitsTransfer -Source $url -Destination $dest
