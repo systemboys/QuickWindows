1588bdcc417741f2f67391aa0250b89225594190
systemboys <systemboys@hotmail.com>
2024-03-08 12:06:04 -0300

    Atualizando o PowerShell no Windows. [0.0.1]

--- Package_Installers/Windows_Session/Update_PowerShell.ps1
+++ Package_Installers/Windows_Session/Update_PowerShell.ps1
@@ -24,22 +24,65 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     exit
 }
 
-# URL do instalador do PowerShell Core
-$installerUrl = "https://aka.ms/install-powershell.ps1"
+# -------------------------------------
+Add-Type -TypeDefinition @"
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
 
-# Caminho para o instalador temporário
-$installerPath = Join-Path $env:TEMP "install-powershell.ps1"
+public class Program
+{
+    public static void Main()
+    {
+        Application.Run(new MyForm());
+    }
+    
+    public class MyForm : Form
+    {
+        public MyForm()
+        {
+            Button button1 = new Button();
+            button1.Text = "Via IRM";
+            button1.Click += new EventHandler(OpenCalculator);
+            
+            Button button2 = new Button();
+            button2.Text = "Via Invoke-WebRequest";
+            button2.Click += new EventHandler(OpenControlPanel);
+            
+            Controls.Add(button1);
+            Controls.Add(button2);
+        }
+        
+        private void OpenCalculator(object sender, EventArgs e)
+        {
+            iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+        }
+        
+        private void OpenControlPanel(object sender, EventArgs e)
+        {
+            # URL do instalador do PowerShell Core
+            $installerUrl = "https://aka.ms/install-powershell.ps1"
+            
+            # Caminho para o instalador temporário
+            $installerPath = Join-Path $env:TEMP "install-powershell.ps1"
+            
+            # Baixar o script de instalação
+            Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+            
+            # Executar o script de instalação
+            & $installerPath
+            
+            # Remover o script de instalação temporário
+            Remove-Item $installerPath -Force
+            
+            # /Script para atualizar o PowerShell para a versão mais recente
+        }
+    }
+}
+"@
 
-# Baixar o script de instalação
-Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
-
-# Executar o script de instalação
-& $installerPath
-
-# Remover o script de instalação temporário
-Remove-Item $installerPath -Force
-
-# /Script para atualizar o PowerShell para a versão mais recente
+[Program]::Main()
+# -------------------------------------
 
 Write-Host "Press any key to continue..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
