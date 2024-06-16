a76020a4f700a18dc95c3f96d5d52bc15337cfbc
systemboys <systemboys@hotmail.com>
2024-06-16 02:06:23 -0300

    Incrementação de Configurações do arquivo JSON no diretório raiz. [0.1.1]

--- Package_Installers/UtilitiesForWindows/ClearTemporaryFiles.ps1
+++ Package_Installers/UtilitiesForWindows/ClearTemporaryFiles.ps1
@@ -13,17 +13,22 @@
 #   - Alteração que mostra a quantidade de arquivos temporários foram apagados.
 # v0.0.3 2024-06-14 às 23h04, Marcos Aurélio:
 #   - Ajuste na largura da janela do terminal Windows PowerShell para 120.
+# v0.0.4 2024-06-16 às 02h05, Marcos Aurélio:
+#   - Incrementação de Configurações do arquivo JSON no diretório raiz.
 #
 # Licença: GPL.
 
+# Configurações
+$configData = Get-Content -Path "./config.json" | ConvertFrom-Json
+
 # Cria uma nova instância do objeto System.Management.Automation.Host.Size
-$size = New-Object System.Management.Automation.Host.Size(120, 30)
+$size = New-Object System.Management.Automation.Host.Size($configData.PowerShellTerminalWidth, $configData.PowerShellTerminalHeight)
 
 # Atribui o novo tamanho à janela do PowerShell
 $host.UI.RawUI.WindowSize = $size
 
 # Define a cor de fundo para preto
-$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.BackgroundColor = $configData.backgroundColor1
 Clear-Host  # Limpa a tela para aplicar a nova cor
 
 # Mensagem de início
