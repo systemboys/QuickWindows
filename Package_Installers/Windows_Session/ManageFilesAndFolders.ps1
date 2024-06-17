de5cb4cc51a69c521887a8fee65750a933297d88
systemboys <systemboys@hotmail.com>
2024-06-16 22:45:33 -0300

    Incrementação de Configurações do arquivo JSON no diretório raiz. [0.1.5]

--- Package_Installers/Windows_Session/ManageFilesAndFolders.ps1
+++ Package_Installers/Windows_Session/ManageFilesAndFolders.ps1
@@ -12,17 +12,22 @@
 #   - Versão inicial, Abrir Gerenciador de Arquivos com Endereço Específico.
 # v0.0.2 2024-06-14 às 23h14, Marcos Aurélio:
 #   - Ajuste na largura da janela do terminal Windows PowerShell para 120.
+# v0.0.3 2024-06-16 às 22h42, Marcos Aurélio:
+#   - Incrementação de Configurações do arquivo JSON no diretório raiz.
 #
 # Licença: GPL.
 
+# Configurações
+$configData = Get-Content -Path "./config.json" | ConvertFrom-Json
+
 # Cria uma nova instância do objeto System.Management.Automation.Host.Size
-$size = New-Object System.Management.Automation.Host.Size(120, 30)
+$size = New-Object System.Management.Automation.Host.Size($configData.PowerShellTerminalWidth, $configData.PowerShellTerminalHeight)
 
 # Atribui o novo tamanho à janela do PowerShell
 $host.UI.RawUI.WindowSize = $size
 
 # Define a cor de fundo para preto
-$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.BackgroundColor = $configData.backgroundColor1
 Clear-Host  # Limpa a tela para aplicar a nova cor
 
 Add-Type -AssemblyName System.Windows.Forms
