e9ba168c58372f57da0cf072210c556a5199b61e
systemboys <systemboys@hotmail.com>
2024-06-30 02:32:21 -0300

    Definindo o título do QW. [8]

--- QuickWindows.cmd
+++ QuickWindows.cmd
@@ -228,15 +228,13 @@ set /a resultado=ano-2008
 for /f "usebackq delims=" %%a in ("config.json") do (
     set "jsonContent=%%a"
 )
-:: Exibir o conteúdo do JSON
-echo %promptWindowTitle%
 :: ------------------------------------
 
 :: Definir o título da janela do Prompt
 TITLE GTi - QuickWindows !lastVersion!
 
 :: Mensagem de entrada do Menu com o resultado
-echo © %ano% - GLOBAL TEC Informática ® - A %resultado% no mercado de Informática - QuickWindows !lastVersion!
+echo © %ano% - GLOBAL TEC Informática ® - A %resultado% no mercado de Informática - QuickWindows !lastVersion! - %promptWindowTitle%
 echo www.gti1.com.br - gti.inf@hotmail.com - systemboys@hotmail.com
 echo QuickWindows.cmd - Executa o menu com várias linhas de comandos para instalação de softwares para Windows
 echo URL: https://github.com/systemboys/QuickWindows.git
