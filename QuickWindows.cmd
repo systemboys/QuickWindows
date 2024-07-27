e7a98e57e1d541d238c75b84127cf15a51825dd6
systemboys <systemboys@hotmail.com>
2024-07-27 18:44:19 -0300

    Teste, ajustando a atualização do QuickWindows para ser feita através do PowerShell e não do CMD. [7]

--- QuickWindows.cmd
+++ QuickWindows.cmd
@@ -234,9 +234,6 @@ cls
 
 chcp 65001 > nul
 
-:: Define a largura e a altura da janela do CMD
-mode con: cols=150 lines=100
-
 :: Mudar a cor do plano de fundo
 powershell -command "$Host.UI.RawUI.BackgroundColor = 'Black'"
 cls
