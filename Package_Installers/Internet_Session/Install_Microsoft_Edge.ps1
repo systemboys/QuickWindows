f1a1b7573777513e0990feacd5632ca1b3dc2b6c
systemboys <systemboys@hotmail.com>
2024-07-31 01:01:13 -0300

    Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON. [6]

--- Package_Installers/Internet_Session/Install_Microsoft_Edge.ps1
+++ Package_Installers/Internet_Session/Install_Microsoft_Edge.ps1
@@ -21,6 +21,8 @@
 #   - Ajuste na remoção do arquivo baixado em Temp, uma condição que verifica a existência do arquivo foi adicionada.
 # v1.2.4 2024-07-28 às 00h06, Marcos Aurélio:
 #   - Registro de logs.
+# v1.3.4 2024-07-31 às 00h59, Marcos Aurélio:
+#   - Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON.
 #
 # Licença: GPL.
 
@@ -34,6 +36,14 @@ if (-not (Test-Path $configPath)) {
 }
 # Importa as configurações do arquivo encontrado
 $configData = Get-Content -Path $configPath | ConvertFrom-Json
+
+# Importa o arquivo de URLs
+$urlsPath = "./urls.json"
+if (-not (Test-Path $urlsPath)) {
+    $urlsPath = "../../urls.json"
+}
+$urlsData = Get-Content -Path $urlsPath | ConvertFrom-Json
+$Install_Microsoft_Edge = $urlsData.Internet[5] # Acessa a URL do pacote
 
 # Cria uma nova instância do objeto System.Management.Automation.Host.Size
 $size = New-Object System.Management.Automation.Host.Size($configData.PowerShellTerminalWidth, $configData.PowerShellTerminalHeight)
@@ -77,7 +87,7 @@ if (Test-Path $directory) {
     Write-Host "File size: 1.52 MB"
 
     # Link do download e o diretório Temp
-    $downloadUrl = "https://github.com/systemboys/_GTi_Support_/raw/main/Windows/Internet/MicrosoftEdgeSetup.exe"
+    $downloadUrl = $Install_Microsoft_Edge
     $downloadPath = "$env:temp\MicrosoftEdgeSetup.exe"
 
     # Faz o download do Microsoft Edge
