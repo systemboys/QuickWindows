d852f6f337bf309c8b1e89467282f63f00b98704
systemboys <systemboys@hotmail.com>
2024-07-31 00:46:13 -0300

    Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON. [4]

--- Package_Installers/Internet_Session/Install_HopToDesk.ps1
+++ Package_Installers/Internet_Session/Install_HopToDesk.ps1
@@ -18,6 +18,8 @@
 #   - Ajuste na remoção do arquivo baixado em Temp, uma condição que verifica a existência do arquivo foi adicionada.
 # v1.2.2 2024-07-28 às 00h02, Marcos Aurélio:
 #   - Registro de logs.
+# v1.3.2 2024-07-31 às 00h44, Marcos Aurélio:
+#   - Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON.
 #
 # Licença: GPL.
 
@@ -31,6 +33,14 @@ if (-not (Test-Path $configPath)) {
 }
 # Importa as configurações do arquivo encontrado
 $configData = Get-Content -Path $configPath | ConvertFrom-Json
+
+# Importa o arquivo de URLs
+$urlsPath = "./urls.json"
+if (-not (Test-Path $urlsPath)) {
+    $urlsPath = "../../urls.json"
+}
+$urlsData = Get-Content -Path $urlsPath | ConvertFrom-Json
+$Install_HopToDesk = $urlsData.Internet[0] # Acessa a URL do pacote
 
 # Cria uma nova instância do objeto System.Management.Automation.Host.Size
 $size = New-Object System.Management.Automation.Host.Size($configData.PowerShellTerminalWidth, $configData.PowerShellTerminalHeight)
@@ -76,7 +86,7 @@ if (Test-Path $directory) {
     Write-Host "File size: 7.27 MB"
 
     # Link do download e o diretório Temp
-    $downloadUrl = "https://github.com/systemboys/_GTi_Support_/raw/main/Windows/Internet/RemoteAccess/HopToDesk.exe"
+    $downloadUrl = $Install_HopToDesk
     $downloadPath = "$env:temp\HopToDesk.exe"
     
     # Faz o download do HopToDesk
