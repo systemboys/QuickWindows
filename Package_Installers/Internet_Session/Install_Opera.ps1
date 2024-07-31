1352236f255ca74865a8f11a7958a2e9114e09ae
systemboys <systemboys@hotmail.com>
2024-07-31 01:07:34 -0300

    Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON. [8]

--- Package_Installers/Internet_Session/Install_Opera.ps1
+++ Package_Installers/Internet_Session/Install_Opera.ps1
@@ -20,6 +20,8 @@
 #   - Ajuste na remoção do arquivo baixado em Temp, uma condição que verifica a existência do arquivo foi adicionada.
 # v1.2.3 2024-07-28 às 00h09, Marcos Aurélio:
 #   - Registro de logs.
+# v1.3.3 2024-07-31 às 01h06, Marcos Aurélio:
+#   - Incrementação de arquivo JSON para URLs, chamada URLs na lista do arquivo JSON.
 #
 # Licença: GPL.
 
@@ -33,6 +35,14 @@ if (-not (Test-Path $configPath)) {
 }
 # Importa as configurações do arquivo encontrado
 $configData = Get-Content -Path $configPath | ConvertFrom-Json
+
+# Importa o arquivo de URLs
+$urlsPath = "./urls.json"
+if (-not (Test-Path $urlsPath)) {
+    $urlsPath = "../../urls.json"
+}
+$urlsData = Get-Content -Path $urlsPath | ConvertFrom-Json
+$Install_Opera = $urlsData.Internet[7] # Acessa a URL do pacote
 
 # Cria uma nova instância do objeto System.Management.Automation.Host.Size
 $size = New-Object System.Management.Automation.Host.Size($configData.PowerShellTerminalWidth, $configData.PowerShellTerminalHeight)
@@ -76,7 +86,7 @@ if (Test-Path $directory) {
     Write-Host "File size: 2.8 MB"
 
     # Link do download e o diretório Temp
-    $downloadUrl = "https://github.com/systemboys/_GTi_Support_/raw/main/Windows/Internet/OperaSetup.exe"
+    $downloadUrl = $Install_Opera
     $downloadPath = "$env:temp\OperaSetup.exe"
     
     # Faz o download do Opera
