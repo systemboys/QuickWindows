1fbe389338d47c74155efb43fb319fa0e3fdc7c4
systemboys <systemboys@hotmail.com>
2024-08-07 00:13:08 -0300

    Opção para Download e execução de Moo0 System Monitor Portable em Utilitários para Windows. [3]

--- Package_Installers/UtilitiesForWindows/Run_MiniToolPartitionWizard64bitPortable.ps1
+++ Package_Installers/UtilitiesForWindows/Run_MiniToolPartitionWizard64bitPortable.ps1
@@ -107,7 +107,7 @@ if (Test-Path $directory) {
     # /Extrair o arquivo compactado (.zip)
     
     # Executar o MiniTool Partition Wizard
-    Start-Process -FilePath "$extractPath\MiniTool-Partition-Wizard-v12-64bit-portable\partitionwizard.exe" -Wait
+    Start-Process -FilePath "$extractPath\MiniTool-Partition-Wizard-v12-64bit-portable\partitionwizard.exe"
 
     # Apagar o arquivo e o diretório
     if (Test-Path "$env:TEMP\$downloadPath") {
